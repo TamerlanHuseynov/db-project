efeb35676ed5e821f2721adae753101e4c5fa92f
Tamerlan <thuseynov2017@gmail.com>
2022-04-14 15:09:30 -0400

    update 2

--- src/test/java/com/cydeo/day2/GettingColumnInformation.java
+++ src/test/java/com/cydeo/day2/GettingColumnInformation.java
@@ -4,7 +4,6 @@ import java.sql.*;
 
 public class GettingColumnInformation {
 
-
     /*
         ResultSet object only store Row data
         It does not store any column information
@@ -28,6 +27,32 @@ public class GettingColumnInformation {
         Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet rs   = stmt.executeQuery("SELECT * FROM EMPLOYEES") ;
 
+        // We want to extract column information from above ResultSet object
+        // First we need to ResultSetMetaData object by calling below method
+        ResultSetMetaData rsmd = rs.getMetaData();
+        // This ResultSetMetaData has 2 methods we care about
+        //  getColumnCount method to get the count of column
+        //  getColumnName or getColumnLabel using 1 based index
+        System.out.println("rsmd.getColumnCount() = " + rsmd.getColumnCount() );
+
+        System.out.println("rsmd.getColumnName(2) = " + rsmd.getColumnName(2));
+
+        // Now we know how to count how many columns we have
+        // and how to get the column name at specific position (starting with 1)
+        // Print out all column names -->> for loop
+        int totalColumnCount = rsmd.getColumnCount() ;
+
+        for (int columnIndex = 1; columnIndex <= totalColumnCount ; columnIndex++) {
+//            System.out.println("rsmd.getColumnName( "+columnIndex+" ) = "
+//                    + rsmd.getColumnName(columnIndex) );
+            System.out.println(rsmd.getColumnName(columnIndex));
+        }
+
+        // HOMEWORK :
+        // NOW YOU KNOW HOW TO GO THROUGH ALL THE COLUMNS
+        // AND HOW TO GO THROUGH ALL THE ROWS
+        // WRITE A PIECE OF CODE THAT GO THROUGH EACH AND EVERY CELL
+        // AND PRINT OUT IT'S VALUE
 
     }
 
